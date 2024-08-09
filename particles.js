f3e77c4dac566c64202682ea5ec696d29d7761f7
Ryan <116501522+p1poca2F3136@users.noreply.github.com>
2024-08-09 16:57:15 -0300

    Atualizar o particles.js

--- particles.js
+++ particles.js
@@ -1,15 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const meteor = document.querySelector('.meteor');
+    const meteorContainer = document.querySelector('.meteor-container');
 
-    for (let i = 0; i < 30; i++) {
+    for (let i = 0; i < 50; i++) {  // Aumenta o número de partículas para um rastro maior
         const particle = document.createElement('div');
         particle.classList.add('particle');
-        particle.style.width = Math.random() * 5 + 'px';
+        particle.style.width = Math.random() * 6 + 'px';
         particle.style.height = particle.style.width;
         particle.style.left = Math.random() * 10 + 'px';
         particle.style.top = Math.random() * 10 + 'px';
-        particle.style.animationDuration = '4s'; // Mais tempo para as partículas caírem
-        particle.style.animationDelay = Math.random() * 1 + 's';
-        meteor.appendChild(particle);
+        particle.style.animationDuration = '4s';  // Partículas caem mais devagar
+        particle.style.animationDelay = Math.random() * 2 + 's';
+        meteorContainer.appendChild(particle);
     }
 });
