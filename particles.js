4319837a626459326d58bb4458eee828fd259634
Ryan <116501522+p1poca2F3136@users.noreply.github.com>
2024-08-09 16:54:10 -0300

    Atualizar o particles.js

--- particles.js
+++ particles.js
@@ -8,8 +8,8 @@ document.addEventListener('DOMContentLoaded', function () {
         particle.style.height = particle.style.width;
         particle.style.left = Math.random() * 10 + 'px';
         particle.style.top = Math.random() * 10 + 'px';
-        particle.style.animationDuration = Math.random() * 1 + 0.5 + 's';
-        particle.style.animationDelay = Math.random() * 0.5 + 's';
+        particle.style.animationDuration = '4s'; // Mais tempo para as partículas caírem
+        particle.style.animationDelay = Math.random() * 1 + 's';
         meteor.appendChild(particle);
     }
 });
